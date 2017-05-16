bda50e492a034881da12452760ab6b013e7c59c2
josegabrielzevallos <jose.zevallos.delgadolaflor@ucsp.edu.pe>
2017-05-16 08:18:28 -0500

    Update listas.cpp

--- trabajoex/listas.cpp
+++ trabajoex/listas.cpp
@@ -3,7 +3,8 @@ using namespace std;
 
 void listas(int *lista1,int *lista2,int *lista3)
 {
-    while((*lista1 =! -1) and (*lista2 =! -1))
+    int *tmp = lista3;
+    while((*lista1 != -1) and (*lista2 =! -1))
     {
         if(*lista1<*lista2)
         {
@@ -18,31 +19,34 @@ void listas(int *lista1,int *lista2,int *lista3)
             lista2++;
         }
     }
+    //cout<<"lista 1"<<*lista1<<endl<<"Lista2"<<*lista2<<endl;
     while(*lista1!=-1)
     {
         *lista3=*lista1;
         lista3++;
         lista1++;
     }
+    //cout<<"lista 1"<<*lista1<<endl<<"Lista2"<<*lista2<<endl;
     while(*lista2!=-1)
     {
         *lista3=*lista2;
         lista3++;
         lista2++;
     }
+    //cout<<"lista 1"<<*lista1<<endl<<"Lista2"<<*lista2<<endl;
     *lista3=-1;
-    while(*lista3=!-1)
+   while(*tmp!=-1)
     {
-        cout<<*lista3<<endl;
-        lista3++;
+        cout<<*tmp<<endl;
+        tmp++;
     }
 }
 
 int main()
 {
-    int arr[5]={4,5,6,8,1};
-    int arri[5]={4,5,6,8,1};
-    int ariii[15];
+    int arr[5]={4,5,6,8,-1};
+    int arri[5]={4,5,6,8,-1};
+    int ariii[15]={};
     listas(arr,arri,ariii);
  return 0;
 }
