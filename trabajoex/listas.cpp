e49546e20b8caa25bd8edd44c1122c24e20244ff
josegabrielzevallos <jose.zevallos.delgadolaflor@ucsp.edu.pe>
2017-06-06 08:21:31 -0500

    Update listas.cpp

--- trabajoex/listas.cpp
+++ trabajoex/listas.cpp
@@ -1,24 +1,22 @@
-#include <iostream>
-using namespace std;
-
 void listas(int *lista1,int *lista2,int *lista3)
 {
     int *tmp = lista3;
-    while((*lista1 != -1) and (*lista2 =! -1))
+    while((*lista1 != -1) and (*lista2 != -1))
     {
         if(*lista1<*lista2)
         {
             *lista3=*lista1;
-            lista3++;
             lista1++;
+            lista3++;
         }
         if(*lista1>*lista2)
         {
             *lista3=*lista2;
-            lista3++;
             lista2++;
+            lista3++;
         }
     }
+
     //cout<<"lista 1"<<*lista1<<endl<<"Lista2"<<*lista2<<endl;
     while(*lista1!=-1)
     {
@@ -39,14 +37,6 @@ void listas(int *lista1,int *lista2,int *lista3)
     {
         cout<<*tmp<<endl;
         tmp++;
-    }
-}
+        }
 
-int main()
-{
-    int arr[5]={4,5,6,8,-1};
-    int arri[5]={4,5,6,8,-1};
-    int ariii[15]={};
-    listas(arr,arri,ariii);
- return 0;
 }
